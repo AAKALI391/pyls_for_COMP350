e0ded7e7eecc7e74e7ff4a59d9f634e83e1f6659
AAKALI391 <aarav_akali.sias22@krea.ac.in>
2024-08-12 22:31:09 +0530

    close #1 added argsparse argument parser and fixed a few bugs in previous code

--- pyls.py
+++ pyls.py
@@ -1,9 +1,37 @@
 import os
 from datetime import datetime
+import argparse
 
+#taken from pyls sketch provided by professor
+parser = argparse.ArgumentParser(
+    prog="pyls",
+    description="Lists files in given or current directory",
+    epilog="Poor man's ls",
+)
+parser.add_argument(
+    "dirname",
+    help="Name of directory to list the contents of",
+    action="store",
+    nargs="?",
+    default=".",
+)
+parser.add_argument(
+    "-l",
+    "--long-format",
+    help="Presents more details about files in columnar format",
+    action="store_true",
+)
+parser.add_argument(
+    "-F",
+    "--filetype",
+    help="""Adds an extra character to the end of the printed
+                            filename that indicates its type.""",
+    action="store_true",
+)
+args = parser.parse_args()
 
 def get_data(dir):
-    if dir == None or dir=="":
+    if dir == None or dir==".":
         directory = os.getcwd()
     else:
         directory = os.getcwd()+"/"+dir
@@ -14,10 +42,12 @@ def get_data(dir):
         type = ""
         if os.path.isfile(filepath):
             type = "f"
+        #checking if executable taken from stack overflow
         if os.path.os.access(filepath, os.X_OK):
             type = "x"
         if os.path.isdir(filepath):
             type = "d"
+            #datetime formatting taken from stack overflow
         data = {"name":filename,"size": metadata.st_size,"last_mod":datetime.fromtimestamp(metadata.st_mtime),"type":type}
         file_data.append(data)
     return file_data
@@ -31,11 +61,11 @@ def format_data(file_meatadata,f,l):
                 i["name"] = i["name"]+"*"
     if l:
         for i in file_meatadata:
-            out_str = i["last_mod"].strftime("%Y-%m-%d %H:%M:%S") + "\t" + str(i["size"]) +"\t"+ i["name"]
+            out_str = i["last_mod"].strftime("%Y-%m-%d %H:%M:%S")    + str(i["size"]).rjust(10) +"\t"+ i["name"]
             print(out_str)
     if not l:
         for i in file_meatadata:
             print(i["name"])
 
-metadata = get_data("")
-print(metadata)
+metadata = get_data(args.dirname)
+format_data(metadata,args.filetype, args.long_format )
